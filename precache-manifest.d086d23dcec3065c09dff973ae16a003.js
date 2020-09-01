720423904d408827cd8c1838b1ff5694350eb099
Timur <timur-reuf@yandex.ru>
2020-09-01 18:53:37 +0400

    Updates

--- precache-manifest.d086d23dcec3065c09dff973ae16a003.js
+++ precache-manifest.d086d23dcec3065c09dff973ae16a003.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2972f42f66be5450ae4e3b0ae9fc2533",
+    "revision": "77d99113f40595d8a490b3849c6eecee",
     "url": "/social-network-demo/index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/social-network-demo/static/css/4.ef87a873.chunk.css"
   },
   {
-    "revision": "a941706733c768c217d0",
+    "revision": "59496d30527fa1d5768c",
     "url": "/social-network-demo/static/css/main.b7ae7f7d.chunk.css"
   },
   {
@@ -32,8 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/social-network-demo/static/js/4.7c27a5c6.chunk.js"
   },
   {
-    "revision": "a941706733c768c217d0",
-    "url": "/social-network-demo/static/js/main.885ec45f.chunk.js"
+    "revision": "59496d30527fa1d5768c",
+    "url": "/social-network-demo/static/js/main.49eed046.chunk.js"
   },
   {
     "revision": "26429b7f8e50a9bdc13d",
